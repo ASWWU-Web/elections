4fff5ff27a427bc09cfdbfe0eb2796e12f3a3809
Stephen Ermshar <stephen.ermshar@gmail.com>
2018-11-03 22:10:11 -0700

    include imports in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Shared components
+import { FooterComponent, NavBarComponent, MobileNavComponent, UserBubbleComponent} from './frontend-shared/shared';
+import { RequestService } from './frontend-shared/shared';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UserBubbleComponent,
+    MobileNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    RequestService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
