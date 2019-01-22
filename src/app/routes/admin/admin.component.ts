8c343b86858154a9d221a730d5bc459bc9738732
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-01-21 17:46:43 -0800

    WIP creating table component

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -17,8 +17,6 @@ export class AdminComponent implements OnInit {
 
   constructor() { }
 
-
-
   ngOnInit() {
   }
 
--- src/app/shared/admin/admin-table/admin-table.component.ts
+++ src/app/shared/admin/admin-table/admin-table.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Type } from '@angular/compiler';
 
 @Component({
   selector: 'app-admin-table',
   templateUrl: './admin-table.component.html',
   styleUrls: ['./admin-table.component.css']
 })
+
+interface ElectionType {
+  id: string;
+  election_type: string;
+  start: string;
+  end: string;
+}
+
+interface PositionType {
+  id: string;
+  position: string;
+  election_type: string;
+  active: boolean; // this may need to be a string
+  order: number; // this may need to be a string
+}
+
+interface CandidateType {
+  id: string;
+  election: string;
+  position: string;
+  username: string;
+  display_name: string;
+}
+
 export class AdminTableComponent implements OnInit {
 
   @Input() viewMode: string;
+  dataType: Type;
+  data: Array<T>;
 
   constructor() { }
 
