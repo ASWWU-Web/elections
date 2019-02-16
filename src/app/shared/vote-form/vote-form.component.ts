b9cbf34b1c47acb6b5e277df0cfa3ac643f1e989
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-02-15 23:02:02 -0800

    working typeahead

--- src/app/shared/vote-form/vote-form.component.ts
+++ src/app/shared/vote-form/vote-form.component.ts
@@ -106,19 +106,18 @@ export class VoteFormComponent implements OnInit {
     );
   }
 
-  search(text$: Observable<string>) {
-    function getNames(query: string, rs) {
-      if (query === '') {
-        return of({results: []});
-      }
-      return rs.get('search/names', {'full_name': query});
+  getNames(query: string) {
+    if (query === '') {
+      return of({results: []});
     }
+    return this.rs.get('search/names', {'full_name': query});
+  }
 
-    let rs = this.rs;
+  search = (text$: Observable<string>) => {
     return text$.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((data) => getNames(data, rs)),
+      switchMap((data) => this.getNames(data)),
       map((data: {results: {username: string, full_name: string}[]}) => {
         return data.results.map((item) => item.username);
       })
