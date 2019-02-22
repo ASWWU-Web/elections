059c296b4c11730c2202ee7ab6f82ad2b7ac4461
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-02-21 19:27:41 -0800

    WIP restructuring

--- src/app/shared/vote-form/vote-form.component.ts
+++ src/app/shared/vote-form/vote-form.component.ts
@@ -8,6 +8,7 @@ import { Observable, of, forkJoin } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
 
 import { Election, Position, PageTransitions } from 'src/app/routes/vote/vote.component';
+import { updateBinding } from '@angular/core/src/render3/instructions';
 
 interface Candidate {
   id: string;
@@ -38,8 +39,11 @@ export class VoteFormComponent implements OnInit {
   @Output() valueChange: EventEmitter<number> = new EventEmitter<number>();
 
   candidates: {info: Candidate, photoUri: string}[] = [];
-  existingVotes: Vote[];
-  stagedVotes: string[];
+  // list of votes includes information for which ones to delete from the server, if the vote exists in the staged votes
+  // it should be indicated to the user that the vote will be deleted when they click submit.
+  // if the vote does not exist in existing votes it might be a good idea to just remove it
+  // from the staged votes list and not indicate that it will be removed.
+  stagedVotes: {vote: Vote, toDelete: boolean}[]; // only ever set toDelete to true if it also exists on the server (vote.id != null)
   formGroup: FormGroup;
   defaultPhoto: string;
   serverErrorText: string;
@@ -52,7 +56,7 @@ export class VoteFormComponent implements OnInit {
 
   ngOnInit() {
     this.setCandidates();
-    this.setExistingVotes();
+    this.stageExistingVotes();
     this.serverErrorText = '';
   }
 
@@ -91,27 +95,74 @@ export class VoteFormComponent implements OnInit {
     );
   }
 
-  setExistingVotes() {
-    function stageExistingVotes(existingVotes: Vote[], This) {
-      for (const vote of existingVotes) {
-        This.stageVote(vote.vote);
-      }
-    }
-
+  stageExistingVotes() {
     const votesObservable = this.rs.get('elections/vote', { position: this.position.id });
     votesObservable.subscribe(
       (data: {votes: Vote[]}) => {
-        this.existingVotes = data.votes;
-        stageExistingVotes(this.existingVotes, this);
+        const existingVotes = data.votes;
+        for (const vote of existingVotes) {
+          this.stageVote(vote);
+        }
       }, (err) => {
       }, () => {
       }
     );
   }
 
-  stageVote(candidateUsername) {
-    if (!this.stagedVotes.includes(candidateUsername)) {
-      this.stagedVotes.push(candidateUsername);
+  indexOfObj(array, propertyPath: string[], value) {
+    function getDeepPropertyValue(object, localPropertyPath: string[]) {
+      // access a property deep in an object
+      let propertyValue = object;
+      for (const property of localPropertyPath) {
+        if (value[property]) {
+          propertyValue = value[property];
+        } else {
+          return;
+        }
+      }
+      return propertyValue;
+    }
+
+    let index = -1;
+    array.forEach((element, i) => {
+      const elementPropertyValue = getDeepPropertyValue(element, propertyPath);
+      if (elementPropertyValue && elementPropertyValue === value) {
+        index = i;
+      }
+    });
+    return index;
+  }
+
+  stageVote(vote: Vote) {
+    // look for a vote in staged votes with the same username as the passed in vote
+    // if no vote is staged by that name, stage the vote, otherwise,
+    // just make sure the vote isn't set to be deleted.
+    const candidateUsername = vote.vote;
+    const stagedVoteIndex = this.indexOfObj(this.stagedVotes, ['vote', 'vote'], candidateUsername);
+    if (stagedVoteIndex === -1) {
+      const voteToStage = {
+        vote: vote,
+        toDelete: false
+      };
+      this.stagedVotes.push(voteToStage);
+    } else {
+      this.stagedVotes[stagedVoteIndex].toDelete = false;
+    }
+  }
+
+  onDeleteVoteButton(stagedVoteIndex) {
+    // if the vote at the index is saved on the server then toggle the toDelete flag
+    // otherwise just remove it from the staging area immediately.
+    const index = stagedVoteIndex;
+    if (index < this.stagedVotes.length && index >= 0) {
+      if (this.stagedVotes[index].vote.id) {
+        // if the id is defined then this vote exists on the server,
+        // so we only toggle the deletion flag
+        this.stagedVotes[index].toDelete = !this.stagedVotes[index].toDelete;
+      } else {
+        // otherwise, just unstage it
+        this.stagedVotes.splice(index, 1);
+      }
     }
   }
 
@@ -136,7 +187,14 @@ export class VoteFormComponent implements OnInit {
   stageWriteIn() {
     const writeIn = this.formGroup.value.writeIn;
     if (writeIn) {
-      this.stageVote(writeIn);
+      const voteToStage: Vote = {
+        id: null, // this should be filled in when we submit if we end up updating a vote
+        election: this.election.id,
+        position: this.position.id,
+        username: null, // this should be filled in when we submit if we end up updating a vote
+        vote: writeIn
+      };
+      this.stageVote(voteToStage);
     }
   }
 
@@ -145,27 +203,44 @@ export class VoteFormComponent implements OnInit {
   }
 
   buildRequestArrayObservable() {
-    let updatableVotes: Vote[] = this.existingVotes;
-    let unsubmittedVotes: string[] = (this.formGroup['controls'].writeInArray['controls']).map((control) => control.value.writeIn);
+    let updatableVotes: {vote: Vote, toDelete: boolean}[] = [];
+    let newVotes: Vote[] = [];
 
-    let requestArray = [];
-    for (let updatableVote of updatableVotes) {
-      let updateVote = Object.assign({}, updatableVote);
-      updateVote.vote = unsubmittedVotes.shift();
-      if (updateVote.vote !== '' && updateVote.vote != null) {
-        requestArray.push(this.rs.put('elections/vote/' + updateVote.id, updateVote));
+    // sort votes into new votes and votes that can be updated or deleted
+    // since toDelete should never be true if a vote is not on the server we could
+    // only sort by toDelete, we sort with existance of ID just to be sure that the vote
+    // exists on the server
+    for (let vote of this.stagedVotes) {
+      if (vote.toDelete && vote.vote.id) {
+        updatableVotes.push(vote);
+      } else {
+        newVotes.push(vote.vote);
       }
     }
 
-    for (let unsubmittedVote of unsubmittedVotes) {
-      let newVote = {
+    let requestArray = [];
+
+    // for each vote to be updated we pop one vote off of the newVotes array, and update (put) the
+    // updatableVote with the new vote candidate username
+    for (const vote of updatableVotes) {
+      let updatableVote = vote.vote;
+      updatableVote.vote = newVotes.shift().vote;
+      requestArray.push(this.rs.put('elections/vote/' + updatableVote.id, updatableVote));
+    }
+
+    // now post the remaining newVotes
+    for (const vote of newVotes) {
+      const newVote = {
         election: this.election.id,
         position: this.position.id,
-        vote: unsubmittedVote
+        vote: vote.id
       };
-      if (newVote.vote !== '') {
-        requestArray.push(this.rs.post('elections/vote', newVote));
-      }
+      requestArray.push(this.rs.post('elections/vote', newVote));
+    }
+
+    // and finally delete any remaining updatable votes still on the server
+    for (const vote of updatableVotes) {
+      requestArray.push(this.rs.delete('elections/vote/' + vote.vote.id));
     }
 
     return forkJoin(requestArray);
