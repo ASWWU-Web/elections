25992540579a0d2d0bd88508f629bc92a74d78a3
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-02-24 16:58:35 -0800

    Added ballot entry history to ballot panel.

--- src/app/shared/admin/admin-ballots/admin-ballots.component.ts
+++ src/app/shared/admin/admin-ballots/admin-ballots.component.ts
@@ -8,6 +8,7 @@ interface Election {
   start: string;
   end: string;
   show_results: string;
+  name: string;
 }
 
 interface Position {
@@ -25,6 +26,15 @@ interface BallotPOST {
   vote: string;
 }
 
+interface Ballot {
+  id: string;
+  election: string;
+  position: string;
+  vote: string;
+  username: string;
+  manual_entry: string;
+}
+
 @Component({
   selector: 'app-admin-ballots',
   templateUrl: './admin-ballots.component.html',
@@ -36,6 +46,7 @@ export class AdminBallotsComponent implements OnInit {
   candidatesData: Candidate[] = [];
   selectedElection: Election = null;
   availablePositions: Position[] = [];
+  ballots: Ballot[] = [];
 
   constructor(private rs: RequestService) { }
 
@@ -44,26 +55,44 @@ export class AdminBallotsComponent implements OnInit {
 
   onSelectElection(election: number): void {
     // get the candidates for the newly selected election
-    const getUrl = 'elections/election/' + this.electionsData[election].id + '/candidate';
-    this.rs.get(getUrl).subscribe((data) => {
+    const candidateUrl = 'elections/election/' + this.electionsData[election].id + '/candidate';
+    this.rs.get(candidateUrl).subscribe((data) => {
       // update candidates array
       this.candidatesData = data.candidates;
     }, (error) => {
       console.error('could not get candidates for selected election');
     });
-    // clear the candidates array
+    // clear the candidates and ballots array
     this.candidatesData = [];
+    this.ballots = [];
     // set the selected election
     this.selectedElection = this.electionsData[election];
+    // get all existing ballots
+    const ballotUrl = 'elections/election/' + this.selectedElection.id + '/ballot';
+    this.rs.get(ballotUrl).subscribe((data) => {
+      this.ballots = data.ballots;
+    }, (error) => {
+      console.error(error);
+    });
     // filter all the available positions
     this.availablePositions = this.positionsData.filter(position => position.election_type === this.selectedElection.election_type);
   }
 
   onSaveBallot(ballot: BallotPOST) {
-    this.rs.post('elections/election/' + ballot.election + '/ballot', ballot).subscribe((data) => {
-      console.log('success');
+    const postUrl = 'elections/election/' + ballot.election + '/ballot';
+    this.rs.post(postUrl, ballot).subscribe((data) => {
+      // add response data to ballots array
+      this.ballots.unshift(data);
     }, (error) => {
       console.error(error);
     });
+  }
+
+  prettyUsername(username: string): string {
+    return username.replace(/\./g, ' ');
+  }
+
+  idToPosition(positionId: string): string {
+    return this.positionsData.find(p => p.id === positionId).position;
   }
 }
