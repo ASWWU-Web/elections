dba9e1ab396dcdcecb21b9d9b8c366e42b35cf24
morini <nicholas.morin@wallawalla.edu>
2019-02-26 14:16:15 -0800

    added listElections function

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RequestService } from 'src/shared-ng/services/services';
+import { ElectionsRequestService } from 'src/shared-ng/services/services';
 import { forkJoin } from 'rxjs';
 import { CURRENT_YEAR } from 'src/shared-ng/config';
 
@@ -29,16 +29,16 @@ export class AdminComponent implements OnInit {
   electionsData: Election[] = [];
   positionsData: Position[] = [];
 
-  constructor(private rs: RequestService) { }
+  constructor(private ers: ElectionsRequestService) { }
 
   ngOnInit() {
     const startDate: string = '20' + CURRENT_YEAR.slice(0, 2) + '-09-01 00:00:00.000000';
     const endDate: string = '20' + CURRENT_YEAR.slice(2, 4) + '-07-01 00:00:00.000000';
-    const electionsObservable = this.rs.get('elections/election', {start_after: startDate, end_before: endDate});
-    const positionsObservable = this.rs.get('elections/position');
+    const electionsObservable = this.ers.listElection({start_after: startDate, end_before: endDate});
+    const positionsObservable = this.ers.get('elections/position');
     forkJoin([electionsObservable, positionsObservable]).subscribe(
-      (data: [{elections: Election[]}, {positions: Position[]}]) => {
-        this.electionsData = data[0].elections;
+      (data: [Election[], {positions: Position[]}]) => {
+        this.electionsData = data[0];
         this.positionsData = data[1].positions;
       }, (err) => {
         window.alert('Unable to fetch data for elections and/or positions\n' + err.error.status);
