2700bbf7f66715c881e1c0e1694ae417011e7962
morini <nicholas.morin@wallawalla.edu>
2019-03-11 15:19:24 -0700

    added listPositions method to admin page

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -20,11 +20,12 @@ export class AdminComponent implements OnInit {
     const startDate: string = '20' + CURRENT_YEAR.slice(0, 2) + '-09-01 00:00:00.000000';
     const endDate: string = '20' + CURRENT_YEAR.slice(2, 4) + '-07-01 00:00:00.000000';
     const electionsObservable = this.ers.listElection({start_after: startDate, end_before: endDate});
-    const positionsObservable = this.ers.get('elections/position');
+    // const positionsObservable = this.ers.get('elections/position');
+    const positionsObservable = this.ers.listPosition();
     forkJoin([electionsObservable, positionsObservable]).subscribe(
-      (data: [Election[], {positions: Position[]}]) => {
+      (data: [Election[], Position[]]) => {
         this.electionsData = data[0];
-        this.positionsData = data[1].positions;
+        this.positionsData = data[1];
       }, (err) => {
         window.alert('Unable to fetch data for elections and/or positions\n' + err.error.status);
       }
