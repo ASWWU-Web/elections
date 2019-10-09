26837e042a2b2585c58c0d214ad690acf6bbde52
morini <nicholas.morin@wallawalla.edu>
2019-10-09 15:32:32 -0700

    Revert "added warning message to user if queue is empty"
    This reverts commit cbe232c891dccb0c1730a60109c9333240b47334.

--- src/app/shared/vote-form/vote-form.component.ts
+++ src/app/shared/vote-form/vote-form.component.ts
@@ -29,7 +29,6 @@ export class VoteFormComponent implements OnInit {
   numVotesToKeep: number;
   disableVoteStaging: boolean;
   serverErrorText: string;
-  alertUser: boolean;
 
   constructor(private fb: FormBuilder, private ers: ElectionsRequestService) {
     this.defaultPhoto = MEDIA_SM + '/' + DEFAULT_PHOTO;
@@ -90,12 +89,6 @@ export class VoteFormComponent implements OnInit {
         }
       }, (err) => {
       }, () => {
-        // disable cast votes button if stagedVotes array length is 0
-        if (this.stagedVotes.length === 0) {
-          this.alertUser = true;
-        } else {
-          this.alertUser = false;
-        }
       }
     );
   }
@@ -159,12 +152,6 @@ export class VoteFormComponent implements OnInit {
     } else {
       this.stagedVotes[stagedVoteIndex].toDelete = false;
     }
-     // disable cast votes button if stagedVotes array length is 0
-     if (this.stagedVotes.length === 0) {
-      this.alertUser = true;
-    } else {
-      this.alertUser = false;
-    }
     this.updateNumVotesToKeep();
   }
 
@@ -189,11 +176,6 @@ export class VoteFormComponent implements OnInit {
       } else {
         this.stageVoteRemoval(index);
       }
-    }
-    if (this.stagedVotes.length === 0) {
-      this.alertUser = true;
-    } else {
-      this.alertUser = false;
     }
   }
 
@@ -237,23 +219,12 @@ export class VoteFormComponent implements OnInit {
       position: this.position.id,
       username: null,
       vote: candidateUsername
-    };
+    }
     this.stageVote(voteToStage);
   }
 
   pageTransition(transition: number) {
-    let i;
-    if (this.alertUser) {
-      i = confirm('There are no votes in the queue. Click + next to write-in to add write-in' +
-              ' or select a candidate. Select ok to exit voting.');
-    }
-
-    if (i === undefined || i === true) {
-      this.valueChange.emit(transition);
-    } else {
-      // do nothing and keep user on current screen
-    }
-
+    this.valueChange.emit(transition);
   }
 
   buildRequestArrayObservable() {
