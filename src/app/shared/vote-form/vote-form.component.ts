8781a1f9744504eb44038d641f6bb61095c32629
morini <nicholas.morin@wallawalla.edu>
2019-10-21 16:14:26 -0700

    changed code back to original

--- src/app/shared/vote-form/vote-form.component.ts
+++ src/app/shared/vote-form/vote-form.component.ts
@@ -1,85 +1,83 @@
-// https://alligator.io/angular/reactive-forms-formarray-dynamic-fields/
+// https://alligator.io/angular/reactive-forms-formarray-dynamic-fields/
 
-import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray, Validators, FormControl } from '@angular/forms';
-import { ElectionsRequestService } from 'src/shared-ng/services/services';
-import { CURRENT_YEAR, MEDIA_SM, DEFAULT_PHOTO } from 'src/shared-ng/config';
-import { Observable, of, forkJoin } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, Validators, FormControl } from '@angular/forms';
+import { ElectionsRequestService } from 'src/shared-ng/services/services';
+import { CURRENT_YEAR, MEDIA_SM, DEFAULT_PHOTO } from 'src/shared-ng/config';
+import { Observable, of, forkJoin } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
 
-import { PageTransitions } from 'src/app/routes/vote/vote.component';
-import { Election, Position, Candidate, Vote, VotePOST } from 'src/shared-ng/interfaces/elections';
+import { PageTransitions } from 'src/app/routes/vote/vote.component';
+import { Election, Position, Candidate, Vote, VotePOST } from 'src/shared-ng/interfaces/elections';
 
 @Component({
-  selector: 'vote-form',
-  templateUrl: './vote-form.component.html',
-  styleUrls: ['./vote-form.component.css']
+  selector: 'vote-form',
+  templateUrl: './vote-form.component.html',
+  styleUrls: ['./vote-form.component.css']
 })
-export class VoteFormComponent implements OnInit {
-  // request data
-  @Input() election: Election;  // the current election
-  @Input() position: Position;  // the list of district positions
-  // completion emitter
-  @Output() valueChange: EventEmitter<number> = new EventEmitter<number>();
-  
-  candidates: {info: Candidate, photoUri: string}[] = [];
-  stagedVotes: {vote: Vote, toDelete: boolean}[]; // only ever set toDelete to true if it also exists on the server (vote.id != null)
-  formGroup: FormGroup;
-  defaultPhoto: string;
-  numVotesToKeep: number;
-  disableVoteStaging: boolean;
-  serverErrorText: string;
-  alertUser: boolean;
+export class VoteFormComponent implements OnInit {
+  // request data
+  @Input() election: Election;  // the current election
+  @Input() position: Position;  // the list of district positions
+  // completion emitter
+  @Output() valueChange: EventEmitter<number> = new EventEmitter<number>();
 
+  candidates: {info: Candidate, photoUri: string}[] = [];
+  stagedVotes: {vote: Vote, toDelete: boolean}[]; // only ever set toDelete to true if it also exists on the server (vote.id != null)
+  formGroup: FormGroup;
+  defaultPhoto: string;
+  numVotesToKeep: number;
+  disableVoteStaging: boolean;
+  serverErrorText: string;
 
-  constructor(private fb: FormBuilder, private ers: ElectionsRequestService) {
-    this.defaultPhoto = MEDIA_SM + '/' + DEFAULT_PHOTO;
-    this.formGroup = new FormGroup({writeIn: new FormControl('')});
-    this.stagedVotes = [];
-    this.numVotesToKeep = 0;
-    this.disableVoteStaging = false;
-  }
+  constructor(private fb: FormBuilder, private ers: ElectionsRequestService) {
+    this.defaultPhoto = MEDIA_SM + '/' + DEFAULT_PHOTO;
+    this.formGroup = new FormGroup({writeIn: new FormControl('')});
+    this.stagedVotes = [];
+    this.numVotesToKeep = 0;
+    this.disableVoteStaging = false;
+  }
 
-  ngOnInit() {
-    this.setCandidates();
-    this.stageExistingVotes();
-    this.serverErrorText = '';
-  }
+  ngOnInit() {
+    this.setCandidates();
+    this.stageExistingVotes();
+    this.serverErrorText = '';
+  }
 
-  setCandidates() {
-    function setCandidatePhoto(username, index, rs, candidates) {
-      const uri = '/profile/' + CURRENT_YEAR + '/' + username;
-      const profileObservable = rs.get(uri);
-      profileObservable.subscribe(
-        (data) => {
-          let photoUri = MEDIA_SM + '/';
-          photoUri += (data.photo !== 'None') ? data.photo : null;
-          candidates[index].photoUri = photoUri;
-        }, (err) => {
-          // TODO (stephen)
-        }, () => {
-          // TODO (stephen)
-        }
-      );
-    }
+  setCandidates() {
+    function setCandidatePhoto(username, index, rs, candidates) {
+      const uri = '/profile/' + CURRENT_YEAR + '/' + username;
+      const profileObservable = rs.get(uri);
+      profileObservable.subscribe(
+        (data) => {
+          let photoUri = MEDIA_SM + '/';
+          photoUri += (data.photo !== 'None') ? data.photo : null;
+          candidates[index].photoUri = photoUri;
+        }, (err) => {
+          // TODO (stephen)
+        }, () => {
+          // TODO (stephen)
+        }
+      );
+    }
 
-    // const getUri = 'elections/election/' + this.election.id + '/candidate';
-    // const getCandidatesObservable = this.ers.get(getUri, {position: this.position.id});
-    const candidateObservable = this.ers.listCandidates(this.election.id, {position: this.position.id});
-    candidateObservable.subscribe(
-      (data) => {
-        const candidates = data.map((item: Candidate) => ({info: item, photoUri: ''}));
-        this.candidates = candidates;
-        this.candidates.forEach((candidate, index) => {
-          setCandidatePhoto(candidate.info.username, index, this.ers, this.candidates);
-        });
-      }, (err) => {
-        // TODO
-      }, () => {
-        // TODO
-      }
-    );
-  }
+    // const getUri = 'elections/election/' + this.election.id + '/candidate';
+    // const getCandidatesObservable = this.ers.get(getUri, {position: this.position.id});
+    const candidateObservable = this.ers.listCandidates(this.election.id, {position: this.position.id});
+    candidateObservable.subscribe(
+      (data) => {
+        const candidates = data.map((item: Candidate) => ({info: item, photoUri: ''}));
+        this.candidates = candidates;
+        this.candidates.forEach((candidate, index) => {
+          setCandidatePhoto(candidate.info.username, index, this.ers, this.candidates);
+        });
+      }, (err) => {
+        // TODO
+      }, () => {
+        // TODO
+      }
+    );
+  }
 
   stageExistingVotes() {
     const votesObservable = this.ers.listVote({ position: this.position.id });
@@ -91,55 +89,48 @@ export class VoteFormComponent implements OnInit {
         }
       }, (err) => {
       }, () => {
-        // disable cast votes button if stagedVotes array length is 0
-        if (this.stagedVotes.length === 0) {
-          this.alertUser = true;
-        } else {
-          this.alertUser = false;
-        }
       }
     );
   }
 
+  indexOfObj(array, propertyPath: string[], value) {
+    function getDeepPropertyValue(object, localPropertyPath: string[]) {
+      // access a property deep in an object
+      let propertyValue = object;
+      for (const property of localPropertyPath) {
+        if (propertyValue[property]) {
+          propertyValue = propertyValue[property];
+        } else {
+          return;
+        }
+      }
+      return propertyValue;
+    }
 
-  indexOfObj(array, propertyPath: string[], value) {
-    function getDeepPropertyValue(object, localPropertyPath: string[]) {
-      // access a property deep in an object
-      let propertyValue = object;
-      for (const property of localPropertyPath) {
-        if (propertyValue[property]) {
-          propertyValue = propertyValue[property];
-        } else {
-          return;
-        }
-      }
-      return propertyValue;
-    }
+    let index = -1;
+    array.forEach((element, i) => {
+      const elementPropertyValue = getDeepPropertyValue(element, propertyPath);
+      if (elementPropertyValue && elementPropertyValue === value) {
+        index = i;
+      }
+    });
+    return index;
+  }
 
-    let index = -1;
-    array.forEach((element, i) => {
-      const elementPropertyValue = getDeepPropertyValue(element, propertyPath);
-      if (elementPropertyValue && elementPropertyValue === value) {
-        index = i;
-      }
-    });
-    return index;
-  }
-
-  updateNumVotesToKeep() {
-    let newNumVotesToKeep = 0;
-    for (let vote of this.stagedVotes) {
-      if (!vote.toDelete) {
-        newNumVotesToKeep += 1;
-      }
-    }
-    this.numVotesToKeep = newNumVotesToKeep;
-    if (this.numVotesToKeep + 1 > this.election.max_votes) {
-      this.disableVoteStaging = true;
-    } else {
-      this.disableVoteStaging = false;
-    }
-  }
+  updateNumVotesToKeep() {
+    let newNumVotesToKeep = 0;
+    for (let vote of this.stagedVotes) {
+      if (!vote.toDelete) {
+        newNumVotesToKeep += 1;
+      }
+    }
+    this.numVotesToKeep = newNumVotesToKeep;
+    if (this.numVotesToKeep + 1 > this.election.max_votes) {
+      this.disableVoteStaging = true;
+    } else {
+      this.disableVoteStaging = false;
+    }
+  }
 
   stageVote(vote: Vote) {
     // early exit, cancel if staging the vote would exceed max_votes
@@ -161,27 +152,21 @@ export class VoteFormComponent implements OnInit {
     } else {
       this.stagedVotes[stagedVoteIndex].toDelete = false;
     }
-     // disable cast votes button if stagedVotes array length is 0
-     if (this.stagedVotes.length === 0) {
-      this.alertUser = true;
-    } else {
-      this.alertUser = false;
-    }
     this.updateNumVotesToKeep();
   }
 
-  stageVoteRemoval(stagedVoteIndex) {
-    // stages a vote removal or removes a vote from stagedVotes if it doesn't exist on the server
-    const index = stagedVoteIndex;
-    if (stagedVoteIndex < this.stagedVotes.length && stagedVoteIndex >= 0) {
-      if (this.stagedVotes[index].vote.id) {
-        this.stagedVotes[index].toDelete = true;
-      } else {
-        this.stagedVotes.splice(index, 1);
-      }
-    }
-    this.updateNumVotesToKeep();
-  }
+  stageVoteRemoval(stagedVoteIndex) {
+    // stages a vote removal or removes a vote from stagedVotes if it doesn't exist on the server
+    const index = stagedVoteIndex;
+    if (stagedVoteIndex < this.stagedVotes.length && stagedVoteIndex >= 0) {
+      if (this.stagedVotes[index].vote.id) {
+        this.stagedVotes[index].toDelete = true;
+      } else {
+        this.stagedVotes.splice(index, 1);
+      }
+    }
+    this.updateNumVotesToKeep();
+  }
 
   onDeleteVoteButton(stagedVoteIndex) {
     const index = stagedVoteIndex;
@@ -192,70 +177,54 @@ export class VoteFormComponent implements OnInit {
         this.stageVoteRemoval(index);
       }
     }
-    if (this.stagedVotes.length === 0) {
-      this.alertUser = true;
-    } else {
-      this.alertUser = false;
-    }
   }
 
-  getNames(query: string) {
-    if (query === '') {
-      return of({results: []});
-    }
-    return this.ers.get('search/names', {'full_name': query});
-  }
+  getNames(query: string) {
+    if (query === '') {
+      return of({results: []});
+    }
+    return this.ers.get('search/names', {'full_name': query});
+  }
 
-  search = (text$: Observable<string>) => {
-    return text$.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((data) => this.getNames(data)),
-      map((data: {results: {username: string, full_name: string}[]}) => {
-        return data.results.map((item) => item.username);
-      })
-    );
-  }
+  search = (text$: Observable<string>) => {
+    return text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((data) => this.getNames(data)),
+      map((data: {results: {username: string, full_name: string}[]}) => {
+        return data.results.map((item) => item.username);
+      })
+    );
+  }
 
-  stageWriteIn() {
-    const writeIn = this.formGroup.value.writeIn;
-    if (writeIn) {
-      const voteToStage: Vote = {
-        id: null, // this should be filled in when we submit if we end up updating a vote
-        election: this.election.id,
-        position: this.position.id,
-        username: null, // this should be filled in when we submit if we end up updating a vote
-        vote: writeIn
-      };
-      this.stageVote(voteToStage);
-    }
-    this.formGroup.reset();
-  }
+  stageWriteIn() {
+    const writeIn = this.formGroup.value.writeIn;
+    if (writeIn) {
+      const voteToStage: Vote = {
+        id: null, // this should be filled in when we submit if we end up updating a vote
+        election: this.election.id,
+        position: this.position.id,
+        username: null, // this should be filled in when we submit if we end up updating a vote
+        vote: writeIn
+      };
+      this.stageVote(voteToStage);
+    }
+    this.formGroup.reset();
+  }
 
-  stageUsername(candidateUsername) {
-    const voteToStage: Vote = {
-      id: null,
-      election: this.election.id,
-      position: this.position.id,
-      username: null,
-      vote: candidateUsername
-    };
-    this.stageVote(voteToStage);
-  }
+  stageUsername(candidateUsername) {
+    const voteToStage: Vote = {
+      id: null,
+      election: this.election.id,
+      position: this.position.id,
+      username: null,
+      vote: candidateUsername
+    }
+    this.stageVote(voteToStage);
+  }
 
   pageTransition(transition: number) {
-    let i;
-    if (this.alertUser) {
-      i = confirm('There are no votes in the queue. Click + next to write-in to add write-in' +
-              ' or select a candidate. Select ok to exit voting.');
-    }
-
-    if (i === undefined || i === true) {
-      this.valueChange.emit(transition);
-    } else {
-      // do nothing and keep user on current screen
-    }
-
+    this.valueChange.emit(transition);
   }
 
   buildRequestArrayObservable() {
